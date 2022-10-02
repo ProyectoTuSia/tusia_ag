5b86b65d419b0d19389027e9296d043e0bcae724
Johan15111 <jolemusp@unal.edu.co>
2022-10-02 11:54:27 -0500

    Added types definition, resolvers and access to authentication microservice

--- MS_access/authenticationAccess.js
+++ MS_access/authenticationAccess.js
@@ -1,1 +1,143 @@
-// Manage REST communication with microservice
+const fetch = require('node-fetch');
+
+async function loginAuthentication(userCredentials) {
+    try {
+        const response = await fetch('http://localhost:8060/user/login', {
+            method: "POST",
+            headers: {
+                'Accept': 'application/json',
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify(userCredentials),
+        });
+
+        if (!response.ok) {
+            throw new Error(`Error! status: ${response.status}`);
+        }
+
+        const result = await response.json();
+        return result.token;
+
+    } catch (err) {
+        console.log(err);
+    }
+}
+
+async function getAllUsers(token) {
+    try {
+        const response = await fetch('http://localhost:8060/user/', {
+            method: "GET",
+            headers: {
+                'Accept': 'application/json',
+                'Content-Type': 'application/json',
+                'Authorization': `Bearer ${token}`
+            },
+        });
+
+        if (!response.ok) {
+            throw new Error(`Error! status: ${response.status}`);
+        }
+
+        const result = await response.json();
+        return result;
+
+    } catch (err) {
+        console.log(err);
+    }
+}
+
+async function getUserById(id, token) {
+    try {
+        const response = await fetch(`http://localhost:8060/user/${id}`, {
+            method: "GET",
+            headers: {
+                'Accept': 'application/json',
+                'Content-Type': 'application/json',
+                'Authorization': `Bearer ${token}`
+            },
+        });
+
+        if (!response.ok) {
+            throw new Error(`Error! status: ${response.status}`);
+        }
+
+        const result = await response.json();
+        return result;
+
+    } catch (err) {
+        console.log(err);
+    }
+}
+
+async function createUser(userDataToCreate, token) {
+    try {
+        const response = await fetch('http://localhost:8060/user/', {
+            method: "POST",
+            headers: {
+                'Accept': 'application/json',
+                'Content-Type': 'application/json',
+                'Authorization': `Bearer ${token}`
+            },
+            body: JSON.stringify(userDataToCreate),
+        });
+
+        if (!response.ok) {
+            throw new Error(`Error! status: ${response.status}`);
+        }
+
+        const result = await response.json();
+        return result;
+
+    } catch (err) {
+        console.log(err);
+    }
+}
+
+async function updateUser(id, userDataToUpdate, token) {
+    try {
+        const response = await fetch(`http://localhost:8060/user/${id}`, {
+            method: "PUT",
+            headers: {
+                'Accept': 'application/json',
+                'Content-Type': 'application/json',
+                'Authorization': `Bearer ${token}`
+            },
+            body: JSON.stringify(userDataToUpdate),
+        });
+
+        if (!response.ok) {
+            throw new Error(`Error! status: ${response.status}`);
+        }
+
+        const result = await response;
+        return result.status;
+
+    } catch (err) {
+        console.log(err);
+    }
+}
+
+async function deleteUser(id, token) {
+    try {
+        const response = await fetch(`http://localhost:8060/user/${id}`, {
+            method: "DELETE",
+            headers: {
+                'Accept': 'application/json',
+                'Content-Type': 'application/json',
+                'Authorization': `Bearer ${token}`
+            },
+        });
+
+        if (!response.ok) {
+            throw new Error(`Error! status: ${response.status}`);
+        }
+
+        const result = await response;
+        return result.status;
+
+    } catch (err) {
+        console.log(err);
+    }
+}
+
+module.exports = { loginAuthentication, getAllUsers, getUserById, createUser, updateUser, deleteUser }
--- graphql_schema/resolvers/authenticationResolvers.js
+++ graphql_schema/resolvers/authenticationResolvers.js
@@ -1,6 +1,159 @@
+const { loginAuthentication, getAllUsers, getUserById, createUser, updateUser, deleteUser } = require("../../MS_access/authenticationAccess");
+
 const authenticationResolvers = {
-// Queries
-  // Mutations
+  Query: {
+    async getAllUsers(parent, args, context)
+    {
+      const users = await getAllUsers(token);
+      return users;
+    },
+
+    async getUserByID(parent, args, context)
+    {
+      const { id } = args;
+      const user = await getUserById(id, token);
+      return user;
+    }
+  },
+
+  Mutation: {
+    async createUser(parent, { input }, context)
+    {
+      const {
+        password,
+        mail,
+        role,
+        fullName,
+        idCard,
+        idIssuingDept,
+        idIssuingPlace,
+        gender,
+        ethnicity,
+        personalMail,
+        mobileNumber,
+        landlineTelephone,
+
+        birthDate,
+        countryBirth,
+        departmentBirth,
+        municipalityBirth,
+        nationality,
+
+        bloodType,
+        rHFactor,
+        ePS,
+
+        nameResponsibleParentFather,
+        firstSurnameFather,
+        secondSurnameFather,
+        documentTypeFather,
+        idNumberFather,
+
+        nameResponsibleParentMother,
+        firstSurnameMother,
+        secondSurnameMother,
+        documentTypeMother,
+        idNumberMother,
+
+        addressOrigin,
+        municipalityOrigin,
+        departmentOrigin,
+        postalCodeOrigin,
+        stratum,
+
+        addressResidence,
+        municipalityResidence,
+        departmentResidence,
+        postalCodeResidence,
+        telephone,
+
+        militaryPassbook,
+      } = input;
+
+      const newUser = {
+        password,
+
+        basicData:{
+          mail,
+          role,
+          fullName,
+          idCard,
+          idIssuingDept,
+          idIssuingPlace,
+          gender,
+          ethnicity,
+          personalMail,
+          mobileNumber,
+          landlineTelephone,
+        },
+
+        birthData:{
+          birthDate,
+          countryBirth,
+          departmentBirth,
+          municipalityBirth,
+          nationality,
+        },
+
+        healthData:{
+          bloodType,
+          rHFactor,
+          ePS,
+        },
+
+        responsible:{
+          responsibleFatherData:{
+            nameResponsibleParentFather,
+            firstSurnameFather,
+            secondSurnameFather,
+            documentTypeFather,
+            idNumberFather,
+          },
+          responsibleMotherData:{
+            nameResponsibleParentMother,
+            firstSurnameMother,
+            secondSurnameMother,
+            documentTypeMother,
+            idNumberMother,
+          }
+        },
+
+        originData:{
+          addressOrigin,
+          municipalityOrigin,
+          departmentOrigin,
+          postalCodeOrigin,
+          stratum,
+        },
+
+        residenceData:{
+          addressResidence,
+          municipalityResidence,
+          departmentResidence,
+          postalCodeResidence,
+          telephone,
+        },
+
+        militarySituation:{
+          militaryPassbook,
+        }
+        };
+
+      const response = await createUser(newUser, token);
+      return response;
+    },
+
+    async deleteUser(parent, { id }, context) {
+      const status = await deleteUser(id, token);
+      return status;
+    },
+
+    async updateUser(parent, { id, input }, context) {     
+      const response = await updateUser(id, input, token);
+      return "response";
+    }
+  }
 
 }
+
 module.exports = { authenticationResolvers }
