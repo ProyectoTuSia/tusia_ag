25db6c54e1563516f70fb0336e8587d35863806f
Julian-Vega-UNAL <55605085+Julian-Vega-UNAL@users.noreply.github.com>
2022-10-02 18:05:08 -0500

    Add files via upload

--- MS_access/courseSearchAccess.js
+++ MS_access/courseSearchAccess.js
@@ -1,1 +1,255 @@
-// Manage REST communication with microservice
+const fetch = require('node-fetch')
+const requestpromisenative = require('request-promise-native')
+const host = 'localhost'
+const getCampus = async () => {
+  const url = 'http://' + host + ':4325/models/campus/'
+  return await fetch(url, {
+    method: 'GET'
+  })
+    .then((response) => {
+      const result = response.json()
+      return result
+    }
+    ).catch(error => console.log(error))
+}
+const getCampusById = async (Id_campus) => {
+  const options = {
+    uri: 'http://' + host + ':4325/models/campus/' + Id_campus,
+    json: true
+  }
+  return await requestpromisenative(options)
+}
+const getFaculty = async () => {
+  const url = 'http://' + host + ':4325/models/faculty/'
+  return await fetch(url, {
+    method: 'GET'
+  })
+    .then((response) => {
+      const result = response.json()
+      return result
+    }
+    ).catch(error => console.log(error))
+}
+const getFacultyById = async (Id_faculty) => {
+  const options = {
+    uri: 'http://' + host + ':4325/models/faculty/' + Id_faculty,
+    json: true
+  }
+  return await requestpromisenative(options)
+}
+const getCareer = async () => {
+  const url = 'http://' + host + ':4325/models/career/'
+  return await fetch(url, {
+    method: 'GET'
+  })
+    .then((response) => {
+      const result = response.json()
+      return result
+    }
+    ).catch(error => console.log(error))
+}
+const getCareerById = async (Id_career) => {
+  const options = {
+    uri: 'http://' + host + ':4325/models/career/' + Id_career,
+    json: true
+  }
+  return await requestpromisenative(options)
+}
+const getSubject = async () => {
+  const url = 'http://' + host + ':4325/models/subject/'
+  return await fetch(url, {
+    method: 'GET'
+  })
+    .then((response) => {
+      const result = response.json()
+      return result
+    }
+    ).catch(error => console.log(error))
+}
+const getSubjectById = async (Id_subject) => {
+  const options = {
+    uri: 'http://' + host + ':4325/models/subject/' + Id_subject,
+    json: true
+  }
+  return await requestpromisenative(options)
+}
+const getTypeTypology = async () => {
+  const url = 'http://' + host + ':4325/models/typetypology/'
+  return await fetch(url, {
+    method: 'GET'
+  })
+    .then((response) => {
+      const result = response.json()
+      return result
+    }
+    ).catch(error => console.log(error))
+}
+const getTypeTypologyById = async (Id_typology) => {
+  const options = {
+    uri: 'http://' + host + ':4325/models/typetypology/' + Id_typology,
+    json: true
+  }
+  return await requestpromisenative(options)
+}
+
+const getGroup = async () => {
+  const url = 'http://' + host + ':4325/models/group/'
+  return await fetch(url, {
+    method: 'GET'
+  })
+    .then((response) => {
+      const result = response.json()
+      return result
+    }
+    ).catch(error => console.log(error))
+}
+const getGroupById = async (Id_group) => {
+  const options = {
+    uri: 'http://' + host + ':4325/models/group/' + Id_group,
+    json: true
+  }
+  return await requestpromisenative(options)
+}
+
+const getPlace = async () => {
+  const url = 'http://' + host + ':4325/models/place/'
+  return await fetch(url, {
+    method: 'GET'
+  })
+    .then((response) => {
+      const result = response.json()
+      return result
+    }
+    ).catch(error => console.log(error))
+}
+const getPlaceById = async (Id_place) => {
+  const options = {
+    uri: 'http://' + host + ':4325/models/place/' + Id_place,
+    json: true
+  }
+  return await requestpromisenative(options)
+}
+
+const getCondition = async () => {
+  const url = 'http://' + host + ':4325/models/condition/'
+  return await fetch(url, {
+    method: 'GET'
+  })
+    .then((response) => {
+      const result = response.json()
+      return result
+    }
+    ).catch(error => console.log(error))
+}
+const getConditionById = async (Id_condition) => {
+  const options = {
+    uri: 'http://' + host + ':4325/models/condition/' + Id_condition,
+    json: true
+  }
+  return await requestpromisenative(options)
+}
+
+const getSchedule = async () => {
+  const url = 'http://' + host + ':4325/models/schedule/'
+  return await fetch(url, {
+    method: 'GET'
+  })
+    .then((response) => {
+      const result = response.json()
+      return result
+    }
+    ).catch(error => console.log(error))
+}
+const getScheduleById = async (Id_Schedule) => {
+  const options = {
+    uri: 'http://' + host + ':4325/models/schedule/' + Id_Schedule,
+    json: true
+  }
+  return await requestpromisenative(options)
+}
+
+const getSubjectsconditions = async () => {
+  const url = 'http://' + host + ':4325/models/subjectcondition/'
+  return await fetch(url, {
+    method: 'GET'
+  })
+    .then((response) => {
+      const result = response.json()
+      return result
+    }
+    ).catch(error => console.log(error))
+}
+const getSubjectsconditionsById = async (Id_Subjectconditions) => {
+  const options = {
+    uri: 'http://' + host + ':4325/models/subjectcondition/' + Id_Subjectconditions,
+    json: true
+  }
+  return await requestpromisenative(options)
+}
+
+const getTypeConditions = async () => {
+  const url = 'http://' + host + ':4325/models/typecondition/'
+  return await fetch(url, {
+    method: 'GET'
+  })
+    .then((response) => {
+      const result = response.json()
+      return result
+    }
+    ).catch(error => console.log(error))
+}
+const getTypeConditionsById = async (Id_types) => {
+  const options = {
+    uri: 'http://' + host + ':4325/models/typecondition/' + Id_types,
+    json: true
+  }
+  return await requestpromisenative(options)
+}
+
+const createOrUpdateGroup = async (group) => {
+  const options = {
+    uri: 'http://' + host + ':4325/models/group/',
+    method: 'POST',
+    body: {
+      Id_group: group.Id_group,
+      Modality: group.Modality,
+      Teacher: group.Teacher,
+      Date_start: group.Date_start,
+      Date_finish: group.Date_finish,
+      Duration: group.Duration,
+      Working_day: group.Working_day,
+      Slots: group.Slots,
+      Id_faculty: group.Id_faculty,
+      Id_subject:group.Id_subject,
+    },
+
+    json: true
+  }
+  return await requestpromisenative(options)
+}
+
+module.exports = {
+  getCampus,
+  getCampusById,
+  getFaculty,
+  getFacultyById,
+  getCareer,
+  getCareerById,
+  getSubject,
+  getSubjectById,
+  getGroup,
+  getGroupById,
+  getPlace,
+  getPlaceById,
+  getTypeTypology,
+  getTypeTypologyById,
+  getSchedule,
+  getScheduleById,
+  getCondition,
+  getConditionById,
+  getSubjectsconditions,
+  getSubjectsconditionsById,
+  getTypeConditions,
+  getTypeConditionsById,
+  createOrUpdateGroup
+}
--- graphql_schema/resolvers/courseSearchResolvers.js
+++ graphql_schema/resolvers/courseSearchResolvers.js
@@ -1,6 +1,91 @@
+const courseSearchAccess = require('../../MS_access/courseSearchAccess')
 const courseSearchResolvers = {
   // Queries
+  Query: {
+
+  async getCampus () {
+    return await courseSearchAccess.getCampus()
+  }, 
+  async getCampusById (_, { Id_campus }) {
+    return await courseSearchAccess.getCampusById(Id_campus)
+  },
+
+  async getFaculty() {
+    return await courseSearchAccess.getFaculty()
+  },
+  async getFacultyById(_, { Id_faculty }) {
+    return await courseSearchAccess.getFacultyById(Id_faculty)
+  },
+
+  async getCareer() {
+    return await courseSearchAccess.getCareer()
+  },
+  async getCareerById(_, { Id_career }) {
+    return await courseSearchAccess.getCareerById(Id_career)
+  },
+
+  async getTypeTypology() {
+    return await courseSearchAccess.getTypeTypology()
+  },
+  async getTypeTypologyById(_, { Id_typology }) {
+    return await courseSearchAccess.getTypeTypologyById(Id_typology)
+  },
+
+  async getSubject() {
+    return await courseSearchAccess.getSubject()
+  },
+  async getSubjectById(_, { Id_subject }) {
+    return await courseSearchAccess.getSubjectById(Id_subject)
+  },
+
+  async getGroup () {
+    return await courseSearchAccess.getGroup ()
+  },
+  async getGroupById(_, { Id_group }) {
+    return await courseSearchAccess.getGroupById(Id_group)
+  },
+
+  async getSchedule () {
+    return await courseSearchAccess.getSchedule ()
+  },
+  async getScheduleById(_, { Id_Schedule }) {
+    return await courseSearchAccess.getScheduleById(Id_Schedule)
+  },
+
+  async getPlace() {
+    return await courseSearchAccess.getPlace ()
+  },
+  async getPlaceById(_, { Id_place }) {
+    return await courseSearchAccess.getPlaceById(Id_place)
+  },
+
+  async getCondition() {
+    return await courseSearchAccess.getCondition ()
+  },
+  async getConditionById(_, { Id_condition }) {
+    return await courseSearchAccess.getConditionById(Id_condition)
+  },
+
+  async getSubjectsconditions() {
+    return await courseSearchAccess.getSubjectsconditions ()
+  },
+  async getSubjectsconditionsById(_, { Id_Subjectconditions }) {
+    return await courseSearchAccess.getSubjectsconditionsById(Id_Subjectconditions)
+  },
+
+  async getTypeConditions() {
+    return await courseSearchAccess.getTypeConditions ()
+  },
+  async getTypeConditionsById(_, { Id_types }) {
+    return await courseSearchAccess.getTypeConditionsById(Id_types)
+  },
   // Mutations
+  },
+  Mutation: {
+    async createOrUpdateGroup (_, { group }) {
+      return await courseSearchAccess.createOrUpdateGroup(group)
+    },
+  }
 
 }
 module.exports = { courseSearchResolvers }
