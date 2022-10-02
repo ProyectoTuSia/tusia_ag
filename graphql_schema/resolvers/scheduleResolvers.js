bd9ac6897ddb1cb86fabf64b20a7811ae0dadc0a
Julian ro <davidro2121@gmail.com>
2022-10-01 21:16:01 -0500

    defining the types

--- graphql_schema/resolvers/scheduleResolvers.js
+++ graphql_schema/resolvers/scheduleResolvers.js
@@ -1,5 +1,10 @@
 const scheduleResolvers = {
   // Queries
+  Query : {
+    getSchedule(){
+      
+    }
+  }
   // Mutations
 }
 module.exports = { scheduleResolvers }
--- graphql_schema/typeDefs/scheduleTypeDefs.js
+++ graphql_schema/typeDefs/scheduleTypeDefs.js
@@ -2,11 +2,32 @@ const { gql } = require('apollo-server-express')
 
 const scheduleTypeDefs = gql`
     # TYPES
-    type User {
-        user : String!
+    type Course {
+        name : String!
+        professor : String!
+        code : Int!
+        credits : Int!
+        building : String!
+        classroom : String!
+        group : Int!
+        typo: String
+        timetable : String!
+    }
+
+    type Schedule {
+        monday : [Course]!
+        tuesday : [Course]!
+        wednesday : [Course]!
+        thursday : [Course]!
+        friday : [Course]!
+        saturday : [Course]!
     }
     # Queries
+    getSchedule(userId : String!) : Schedule!
 
     # Mutations
+    mutation {
+        putSchedule( userId: String!, monday : [Course]!, tuesday : [Course]!, wednesday : [Course]!, thursday: [Course]!, friday : [Course]!, saturday : [Course]! )
+    }
 `
 module.exports = { scheduleTypeDefs }
