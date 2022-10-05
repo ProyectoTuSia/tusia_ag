7deb0584fb172ef67916c7c2af1ff8d6c1d31915
Julian ro <davidro2121@gmail.com>
2022-10-05 02:06:33 -0500

    integrating the queue with the business logic

--- RabbitAccess/inscriptionScheduleQueue.js
+++ RabbitAccess/inscriptionScheduleQueue.js
@@ -1,7 +1,7 @@
 const amqp = require('amqplib/callback_api');
 
 // this function should receive the body of th schedule
-const createSchedule = ()=> {amqp.connect('amqp://146.148.79.102:5672', function(error0, connection) {
+const createSchedule = (schedule)=> {amqp.connect('amqp://146.148.79.102:5672', function(error0, connection) {
   if (error0) {
     throw error0;
   }
@@ -11,19 +11,18 @@ const createSchedule = ()=> {amqp.connect('amqp://146.148.79.102:5672', function
     }
     let queue = 'createScheduleQueue';
     // the schedule body
-    let msg = 'Hello zzzz'; 
+    // let msg = 'Hello zzzz'; 
 
     channel.assertQueue(queue, {
       durable: false
     });
 
-    channel.sendToQueue(queue, Buffer.from(msg));
-    console.log(" [x] Sent %s", msg);
+    channel.sendToQueue(queue, Buffer.from(schedule));
+    console.log(" [x] schedule sent  %s", schedule);
   });
 
   setTimeout(function() {
     connection.close();
-    process.exit(0);
 }, 500);
 
 });}
--- RabbitAccess/usingFunction.js
+++ RabbitAccess/usingFunction.js
@@ -1,4 +1,73 @@
 const createSchedule = require('./inscriptionScheduleQueue')
 
-console.log("hola")
-createSchedule.createSchedule()
+schedule = {
+    "userId": "jurinconor",
+    "monday": [
+        {
+            "courseId":78278,
+            "name" : "Calculo integral",
+            "professor": "Santiago Rodriguez",
+            "credits": 3,
+            "building":"El viejo",
+            "classroom":"410",
+            "group":5,
+            "type": "Optativa",
+            "timetable": "16-18"
+        }
+    ],
+    "tuesday": [
+        {
+            "courseId":7875,
+            "name" : "Calculo Integral",
+            "professor": "Ivan Morales",
+            "credits": 3,
+            "building":"Aulas de Ingenieria",
+            "classroom":"76",
+            "group":6,
+            "type": "Obligatoria",
+            "timetable": "9-11"
+        }
+    ],
+    "wednesday": [
+        {
+            "courseId":78278,
+            "name" : "Ingenieria de software 2",
+            "professor": "Carlos Amaya",
+            "credits": 3,
+            "building":"El viejo",
+            "classroom":"420",
+            "group":5,
+            "type": "Optativa",
+            "timetable": "16-18"
+        }
+    ],
+    "thursday": [
+        {
+            "courseId":7875,
+            "name" : "Calculo Integral",
+            "professor": "Ivan Morales",
+            "credits": 3,
+            "building":"Aulas de Ingenieria",
+            "classroom":"79",
+            "group":6,
+            "type": "Obligatoria",
+            "timetable": "9-11"
+        }
+    ],
+    "friday": [],
+    "saturday": [
+        {
+            "courseId":78278,
+            "name" : "Astronomia para todos",
+            "professor": "Triviño",
+            "credits": 3,
+            "building":"Postgrados",
+            "classroom":"420",
+            "group":5,
+            "type": "Optativa",
+            "timetable": "16-18"
+        }
+    ]
+}
+schedule = JSON.stringify(schedule)
+createSchedule.createSchedule(schedule)
