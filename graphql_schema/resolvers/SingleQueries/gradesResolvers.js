37289c5f474f0812c766eb95f61c668e31e6d441
Fabian Castro <66381618+facaslo97@users.noreply.github.com>
2022-10-28 19:38:56 -0500

    Minor changes

--- graphql_schema/resolvers/SingleQueries/gradesResolvers.js
+++ graphql_schema/resolvers/SingleQueries/gradesResolvers.js
@@ -8,8 +8,8 @@ const gradesResolvers = {
       retrievedGrades = retrievedGrades.results
       retrievedGrades.map((grade) => {
         grade.weight = grade.weight.low
-        if (grade.value.low !== undefined) {
-          grade.value = grade.value.low
+        if (grade.grade.low !== undefined) {
+          grade.grade = grade.grade.low
         }
         delete Object.assign(grade, { value: grade.grade }).grade
         return grade
