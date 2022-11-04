c08082560284fe3898f4bd3a988de731171f425e
Fabian Castro <facaslo.99@gmail.com>
2022-11-04 15:51:02 -0500

    Https connection was established

--- app.js
+++ app.js
@@ -1,22 +1,31 @@
 const { ApolloServer } = require('apollo-server-express')
 const express = require('express')
 const printRequestInfo = require('./middleware/printRequestInfo.js')
+const cors = require('cors')
+const https = require('https')
+const fs = require('fs')
+
 const typeDefs = require('./graphql_schema/typeDefs/typeDefs')
 const resolvers = require('./graphql_schema/resolvers/resolvers')
+
 const app = express()
 const server = new ApolloServer({ typeDefs, resolvers })
-const cors = require('cors')
-// Routes
 
-// Configuración del servidor, puerto 3080
-app.set('port', 3080)
 // Middlewares
 app.use(printRequestInfo)
-app.use(cors())
+app.use('/graphql', cors())
 
-server.start().then(res => {
+// Configuración del servidor https, puerto 443
+const httpsServer = https.createServer({
+  cert: fs.readFileSync('./certificates/server.crt'),
+  key: fs.readFileSync('./certificates/key.pem')
+}, app)
+
+// await new Promise((resolve) => httpsServer.listen({ port: 443 }, resolve))
+
+server.start().then(async (resolve) => {
   server.applyMiddleware({ app, path: '/' })
-  app.listen({ port: 3001 }, () =>
-    console.log('Gateway API running at port: 3001')
+  await httpsServer.listen({ port: 443 }, () =>
+    console.log('Gateway API running at port: 443')
   )
 })
